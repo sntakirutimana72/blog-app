4bfbdc7a145faf216f656e86fd7495da16d98710
Steve <sntakirutimana72@gmail.com>
2022-09-15 17:31:40 +0200

    Add validations and specs for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_three_posts
     posts.order(created_at: :desc).first(3)
   end
