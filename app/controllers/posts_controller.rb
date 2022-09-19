594e33deec14e895f7459cf3b1b1a2a5e3b68261
Steve <sntakirutimana72@gmail.com>
2022-09-19 17:19:14 +0200

    Add controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.where(author_id: params[:user_id])
+    @author = @posts ? @posts.first.author : {}
+  end
+
+  def show
+    @post = Post.where(author_id: params[:user_id], id: params[:id]).first
+  end
 end
