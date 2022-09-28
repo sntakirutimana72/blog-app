2ec69bf40d9146806d4bd1c7f89c3c065ae2f1d2
Steve <sntakirutimana72@gmail.com>
2022-09-28 10:50:03 +0200

    Add specs for features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,19 @@ class PostsController < ApplicationController
     @posts = Post
       .includes(:author)
       .where(author_id: params[:user_id])
-    author = @posts ? @posts.first.author : {}
+    author = User.find(params[:user_id])
+
     respond_to do |res|
-      res.html { render(:index, locals: { author: }) }
+      res.html { render(:index, locals: { author:, all: nil }) }
     end
   end
 
   def show
     @post = Post.where(author_id: params[:user_id], id: params[:id]).first
+
+    respond_to do |res|
+      res.html { render(:show, locals: { all: true }) }
+    end
   end
 
   def new
@@ -22,7 +27,7 @@ class PostsController < ApplicationController
   def create
     respond_to do |res|
       res.html do
-        if Post.create(**post_params)
+        if Post.new(post_params).save
           flash[:success] = 'Post saved successfully!'
           redirect_to(user_path(current_user))
         else
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def zero_comments?(post)
     post.comments_counter.nil? || post.comments_counter.zero?
   end
+
+  def which_comments(post, all)
+    all ? post.comments.includes(:author) : post.recent_five_comments
+  end
 end
